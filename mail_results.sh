fe5cd46d1bfbe36bcc337aa33aa792470433e992
Michael Galloy <mgalloy@gmail.com>
2017-12-17 19:27:06 -0700

    Set return address properly for 'make pipe' results

--- mail_results.sh
+++ mail_results.sh
@@ -4,4 +4,4 @@ CONFIG=$(basename $1)
 TMP_FILENAME=$2
 EMAIL=$3
 
-cat ${TMP_FILENAME} | mail -s "Done processing pipeline with ${CONFIG}" ${EMAIL}
+cat ${TMP_FILENAME} | mail -s "Done processing pipeline with ${CONFIG}" -r "$(whoami)@ucar.edu" ${EMAIL}
